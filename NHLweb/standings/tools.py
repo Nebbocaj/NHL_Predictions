d6337dbecf5be806da8f9cc39f049e42dff55e7c
Jacob Hendricks <jbhendri@iu.edu>
2023-05-24 14:15:56 -0400

    added very basic standings ordering

---
+++ NHLweb/standings/tools.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+API_URL = "https://statsapi.web.nhl.com/api/v1"
+def get_teams():
+    
+    response = requests.get(API_URL + "/standings", params={"Content-Type": "application/json"})
+    data = response.json()
+    
+    team_list = []
+    
+    for div in range(4):
+        for i in range(8):
+            
+            team_list.append([data["records"][div]["teamRecords"][i]["team"]["name"],
+                              data["records"][div]["teamRecords"][i]["points"]])
+            # print(data["records"][div]["teamRecords"][i]["team"]["name"],
+            #       data["records"][div]["teamRecords"][i]["leagueRecord"]["wins"],
+            #       data["records"][div]["teamRecords"][i]["leagueRecord"]["losses"],
+            #       data["records"][div]["teamRecords"][i]["leagueRecord"]["ot"],
+            #       data["records"][div]["teamRecords"][i]["points"],
+            #       data["records"][div]["teamRecords"][i]["regulationWins"],
+            #       data["records"][div]["teamRecords"][i]["row"],
+            #       data["records"][div]["teamRecords"][i]["goalsScored"],
+            #       data["records"][div]["teamRecords"][i]["goalsAgainst"],
+            #       data["records"][div]["teamRecords"][i]["goalsScored"] - data["records"][div]["teamRecords"][i]["goalsAgainst"])
+    return team_list
--- NHLweb/standings/views.py
+++ NHLweb/standings/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Team
+
+from .tools import get_teams
+
+def home(request):
+    
+    all_teams = Team.objects.all()
+    
+    #Make sure that there are exactly 32 teams at all times.
+    if len(all_teams) != 32:
+        all_teams.delete()
+        for t in get_teams():
+            new_team = Team(name = t[0], points = t[1])
+            new_team.save()
+            
+    #Retrieve all teams and order them by points.
+    #THIS WILL CHANGE LATER WHEN THERE IS A BETTER ORGANIZATION SYSTEM
+    team_list = Team.objects.order_by("-points")
+    output = ", ".join([str(q.name) + " " +  str(q.points) for q in team_list])
+    
+    return HttpResponse(output)
